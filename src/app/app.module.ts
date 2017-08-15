024463e507568c6e7e6b4e432aaa4e613e9130e6
Marat <marat@iquitdesign.com>
2017-08-14 23:43:40 -0700

    add feed view and switch to scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FeedViewComponent } from './feed-view/feed-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedViewComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/feed-view/feed-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feed-view',
+  templateUrl: './feed-view.component.html',
+  styleUrls: ['./feed-view.component.scss']
+})
+export class FeedViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
